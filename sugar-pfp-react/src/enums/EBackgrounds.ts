3c7f3c4a006984563f1cd0cc8cca96abafbb7409
Bryan Santos <bryanjared.dev@gmail.com>
2024-12-02 21:49:52 -0400

    Deleted base and fixed some images

--- sugar-pfp-react/src/enums/EBackgrounds.ts
+++ sugar-pfp-react/src/enums/EBackgrounds.ts
@@ -1,6 +1,6 @@
 export const EBackgrounds = [
-    "../../public/assets/images/backgounds/1-ardillas-512--chillguy.png",
-    "../../public/assets/images/backgounds/3-ardillas-512-pnt.png",
-    "../../public/assets/images/backgounds/4-ardilla-JB-bg.png",
-    "../../public/assets/images/backgounds/4-ardilla-mostaza-bg.png",
+    "assets/images/backgounds/1-ardillas-512--chillguy.png",
+    "assets/images/backgounds/3-ardillas-512-pnt.png",
+    "assets/images/backgounds/4-ardilla-JB-bg.png",
+    "assets/images/backgounds/4-ardilla-mostaza-bg.png",
 ]
